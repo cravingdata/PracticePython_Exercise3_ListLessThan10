08f9c5bf016cd9cfacc1026c465ac821bb87307c
cravingdata <32245139+cravingdata@users.noreply.github.com>
2017-11-25 21:36:39 -0500

    Add files via upload
    Asking user input, and printing numbers from list that are below the user's input.

---
+++ PracticePython_Exc3_ListLessThan10_addon2.py
@@ -0,0 +1,9 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+ask = float(raw_input("Enter a number: "))
+for number in a:
+    if number < ask:
+        print number
+
+#computer asks user for integer/float.
+#number in list compared with integer/float.
+#computer prints only number less than user input.
